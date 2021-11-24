bf986171a3a43c1aac1b4b87bf01757ce88fe541
Kokoluc7 <viky7u7.5@gmail.com>
2021-11-23 18:07:55 -0700

    sfml adde

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,22 @@
 #include<iostream>
+#include<SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout <<"KK"<< std::endl;
-    return 0;
+  sf::RenderWindow* window{new sf::RenderWindow(sf::VideoMode(800, 800), "Game Engine")};
+  sf::Event* event{new sf::Event()};
+
+  while (window->isOpen())
+  {
+    while (window->pollEvent(*event))
+    {
+      if(event->type == sf::Event::Closed)
+      {
+        window->close();
+      }
+    }
+
+  }
+
+  return 0;
 }
